bc62e9ab2d4061609e917eda8189d4f792279ae0
evgenok <popovevgen18042001@mail.ru>
2022-02-18 21:14:55 +0300

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,10 @@
 $(function() {
+   $('.slider-blog__inner').slick({
+      dots: true,
+      prevArrow: '<button type="button" class="slick-prev"><img src="images/portfolio/arrow-left.svg"></button>' , 
+      nextArrow: '<button type="button" class="slick-next"><img src="images/portfolio/arrow-right.svg"></button>'
+   });
+
     var mixer = mixitup('.portfolio__content');
-})
+
+});
