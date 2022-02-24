1d6befd755cb6e96ed020ef27e3a5088e3e2ca15
evgenok <popovevgen18042001@mail.ru>
2022-02-24 21:33:11 +0300

    adaptive finished

--- app/js/main.js
+++ app/js/main.js
@@ -2,7 +2,15 @@ $(function() {
    $('.slider-blog__inner').slick({
       dots: true,
       prevArrow: '<button type="button" class="slick-prev"><img src="images/portfolio/arrow-left.svg"></button>' , 
-      nextArrow: '<button type="button" class="slick-next"><img src="images/portfolio/arrow-right.svg"></button>'
+      nextArrow: '<button type="button" class="slick-next"><img src="images/portfolio/arrow-right.svg"></button>',
+      responsive: [
+         {
+            breakpoint: 769,
+            settings: {
+               arrows: false
+            }
+         },
+      ]
    });
 
    
