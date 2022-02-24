a236679feee2c23bd4ed2106c611fba4793f10f8
evgenok <popovevgen18042001@mail.ru>
2022-02-24 22:26:48 +0300

    adaptive finished

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,18 @@
 $(function() {
+   $(".menu a").on("click", function (event) {
+      //отменяем стандартную обработку нажатия по ссылке
+      event.preventDefault();
+
+      //забираем идентификатор блока с атрибута href
+      var id = $(this).attr('href'),
+
+         //узнаем высоту от начала страницы до блока на который ссылается якорь
+         top = $(id).offset().top;
+
+      //анимируем переход на расстояние - top за 1500 мс
+      $('body,html').animate({ scrollTop: top }, 1500);
+   });
+
    $('.slider-blog__inner').slick({
       dots: true,
       prevArrow: '<button type="button" class="slick-prev"><img src="images/portfolio/arrow-left.svg"></button>' , 
@@ -15,10 +29,12 @@ $(function() {
 
    
 
-    $('.menu-btn').on('click', function(){
+    $('.menu-btn, .menu a').on('click', function(){
        $('.menu__list').toggleClass('menu__list--active');
     });
 
+
+    
    var mixer = mixitup('.portfolio__content');
 
 });
